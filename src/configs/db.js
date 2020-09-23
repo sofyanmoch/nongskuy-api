8bb455abfc99b422dc9d31dce1e07520527d7b3c
sofyan-dev <mchsfyan17@gmail.com>
2020-09-23 09:37:26 +0700

    fix

--- src/configs/db.js
+++ src/configs/db.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 const connection = mysql.createConnection({
     host: process.env.host,
     user: process.env.user, 
+    password: process.env.password,
     database: process.env.database
 })
 
