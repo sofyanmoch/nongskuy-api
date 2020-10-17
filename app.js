81d6b78e34e67edc240b4c5f9011be249cfff63c
sofyan-dev <mchsfyan17@gmail.com>
2020-10-17 22:06:17 +0700

    try last

--- app.js
+++ app.js
@@ -10,10 +10,7 @@ const path = require('path')
 app.set('views', path.join(__dirname,'src/views'))
 app.set('view engine', 'ejs')
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 3bdb6d5e1f6eebfdf269cbe22ab604e75bbec381
 app.use(express.static('asset/img/'))
 app.use(express.static('src/views'))
 app.use(bodyParser.json())
@@ -21,21 +18,15 @@ app.use(bodyParser.urlencoded({ extended: false }))
 const { port }  = require('./src/helpers/env')
 app.use(cors())
 
-<<<<<<< HEAD
+
 app.use(express.static(path.join(__dirname, './dist')))
 app.use('*',(req,res) => {
     res.sendFile(__dirname, './dist/index.html')
 })
-=======
-// app.use(express.static(path.join(__dirname, './dist')))
-// app.use('*',(req,res) => {
-//     res.sendFile(__dirname, './dist/index.html')
-// })
->>>>>>> 3bdb6d5e1f6eebfdf269cbe22ab604e75bbec381
 
-// app.get('/*', (req,res) => {
-//     res.sendFile(path.join(__dirname, './dist/index.html'))
-// })
+app.get('/*', (req,res) => {
+    res.sendFile(path.join(__dirname, './dist/index.html'))
+})
 
 
 app.use('/',routerNavigation)
