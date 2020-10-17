24c52b01e4e52b2a620bedeef068348f718f31f1
sofyan-dev <mchsfyan17@gmail.com>
2020-10-17 12:44:38 +0700

    add front end

--- app.js
+++ app.js
@@ -10,12 +10,20 @@ const path = require('path')
 app.set('views', path.join(__dirname,'src/views'))
 app.set('view engine', 'ejs')
 
+app.use(express.static(path.join(__dirname, './dist')))
 app.use(express.static('asset/img/'))
 app.use(express.static('src/views'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 const { port }  = require('./src/helpers/env')
 app.use(cors())
+
+app.use('*', (req,res) => {
+    res.sendFile(__dirname, './dist/index.html')
+})
+app.get('/*',(req,res) => {
+    res.sendFile(path.join(__dirname, './dist/index.html'))
+})
 
 app.use('/',routerNavigation)
 app.listen( port , () => {
